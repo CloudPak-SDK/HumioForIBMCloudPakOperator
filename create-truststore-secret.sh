0bd649e986e851499cee15b0d2feb885f4feb43e
Zach Robinson <ZachRobinson@ibm.com>
2021-02-12 10:38:18 -0600

    fix script

--- create-truststore-secret.sh
+++ create-truststore-secret.sh
@@ -1,5 +1,5 @@
-#!bash
+#!/bin/bash
 
 oc -n ibm-common-services get secret cs-ca-certificate-secret -o jsonpath={.data."ca\.crt"} | base64 -d > ca.crt
-keytool -import -trustcacerts -alias cp -file ca.crt -keystore truststore.jks -storepass changeit
+keytool -import -trustcacerts -alias cp -file ca.crt -keystore truststore.jks -storepass changeit -noprompt
 oc create secret generic truststore --from-file=truststore.jks
